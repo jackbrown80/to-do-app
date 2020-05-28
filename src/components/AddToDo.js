2a4a5df87aed18491627b86c610b1c176fade928
Jack Brown <brown_jack80@hotmail.com>
2020-05-28 22:47:28 +0100

    refactored with hooks

--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -1,37 +1,38 @@
 import React from 'react'
 
-class AddToDo extends React.Component {
-  toDoRef = React.createRef()
+function AddToDo(props) {
+  const toDoRef = React.createRef()
 
-  createToDo = (e) => {
+  const createToDo = (e) => {
     // 1. Stop form from submitting
     e.preventDefault()
     // 2. Create a ToDo object containing the text, have done an object so other things like date created can be added at a later date
     const todo = {
-      text: this.toDoRef.current.value,
+      text: toDoRef.current.value,
     }
-    // 3. Pass todo upto App to be added to state
-    this.props.addToDo(todo)
+    // 3. Use hook created in App.js to update the state, take the previouse state and the new todo to this
+    props.updateTodos((prevState) => ({
+      ...prevState,
+      [`ToDo${Date.now()}`]: todo,
+    }))
     // 4. Reset form
     e.currentTarget.reset()
   }
-  render() {
-    return (
-      <form action="" onSubmit={this.createToDo} className="to-do-form">
-        <input
-          type="text"
-          name="to-do-item"
-          ref={this.toDoRef}
-          placeholder="Start typing here..."
-          className="to-do-input"
-          autoComplete="off"
-        />
-        <button type="submit" className="to-do-submit">
-          +
-        </button>
-      </form>
-    )
-  }
+  return (
+    <form action="" onSubmit={createToDo} className="to-do-form">
+      <input
+        type="text"
+        name="to-do-item"
+        ref={toDoRef}
+        placeholder="Start typing here..."
+        className="to-do-input"
+        autoComplete="off"
+      />
+      <button type="submit" className="to-do-submit">
+        +
+      </button>
+    </form>
+  )
 }
 
 export default AddToDo
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,26 +1,26 @@
 import React from 'react'
 
-class ToDo extends React.Component {
-  render() {
-    return (
-      <div className="to-do-item">
-        <input
-          className="to-do-checkbox"
-          type="checkbox"
-          name="complete"
-          id=""
-        />
-        <p className="to-do-text">{this.props.text}</p>
-        <button
-          className="to-do-remove"
-          onClick={() => this.props.removeToDo(this.props.index)}
-        >
-          &times;
-        </button>
-      </div>
-    )
+function ToDo(props) {
+  const removeToDo = (prevState) => {
+    //1. Take a copy of previous state
+    const newToDos = { ...prevState }
+    //2. Remove the todo
+    delete newToDos[props.index]
+    //3. Return new todo object
+    return newToDos
   }
+  return (
+    <div className="to-do-item">
+      <input className="to-do-checkbox" type="checkbox" name="complete" id="" />
+      <p className="to-do-text">{props.text}</p>
+      <button
+        className="to-do-remove"
+        onClick={() => props.updateTodos((prevState) => removeToDo(prevState))}
+      >
+        &times;
+      </button>
+    </div>
+  )
 }
-
 
 export default ToDo
