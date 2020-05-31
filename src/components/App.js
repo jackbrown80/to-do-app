b110bd6d7cef43207b2031b17fd31849947ec3f5
Jack Brown <brown_jack80@hotmail.com>
2020-05-31 11:04:16 +0100

    added local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AddToDo from './AddToDo'
 import ToDo from './ToDo'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
-function App(props) {
-  // Adds a 'todos' property to state object and creates addToDo function which is used to update the state
-  const [todos, updateTodos] = useState({})
+function App() {
+  // Get intial todos state from local storage, if it doesn't exist set it to 0. Parse turns it from a string to an object
+  const initialTodos = JSON.parse(window.localStorage.getItem('todos')) || 0
+  // Adds a 'todos' property to state object and creates addToDo function which is used to update the state. Intital state of todos is set to the local storage
+  const [todos, updateTodos] = useState(initialTodos)
+
+  // Each time App rerenders update local storage with the new state
+  useEffect(() => {
+    // Stringify turns the object into a string so it can be stored correctly
+    window.localStorage.setItem('todos', JSON.stringify(todos))
+  })
 
   return (
     <div className="app">
